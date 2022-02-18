7ef444015b576dd3a9016e2b94698422b3a84332
John Flaneur <johnflaneur@gmail.com>
2022-02-18 00:20:38 -0300

    Backend coder ejercicio 1

--- Ejercicio 1.js
+++ Ejercicio 1.js
@@ -6,8 +6,8 @@ class User{
     constructor(name,lastName, books = [], pets = []){
         this.name = name
         this.lastName = lastName
-        this.books = getBooks(books)
-        this.pets = getPets(pets)
+        this.books = books
+        this.pets = pets
 
     }
 
@@ -36,6 +36,13 @@ class User{
 
 let Flaneur = new User('John', 'Flaneur', [{bookName: 'Hobbit', autor: 'J.R.R Tolkien'}], ['Gato']);
 
+john.addPet('Búho');
+john.addBook({bookName: 'Señor de los Anillos, Las Dos Torres', autor: 'J.R.R Tolkien'});
 
+
+console.log(`Hola soy ${john.getFullName()}
+tengo ${john.countPets()} mascotas y son un ${john.petsNames()[0]}
+ y un ${john.petsNames()[1]} siempre me acompañan y están cerca de mí, sobretodo
+ cuando me siento a leer un poco de dos libros nuevos que me compré que son: ${john.bookName()[0]} y ${john.bookName()[1]}`)
 
 
